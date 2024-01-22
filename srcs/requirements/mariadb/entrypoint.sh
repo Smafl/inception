747c9873e449b475b020ca48a61223d1cfafacac
Lena <lennykulichkova@gmail.com>
2024-01-22 16:34:40 +0100

    run mariadb container, create a wordpress user in mariadb

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-echo "HELLO"
-# mysql -u root -p"$ROOT_PASS" <<EOSQL
-# CREATE DATABASE ${WP_DB_NAME};
-# CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
-# GRANT ALL PRIVILEGES ON '${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';
-# FLUSH PRIVILEGES;
-#EOSQL
+
+# Wait for MariaDB to be ready
+while ! mysqladmin ping -h"$MYSQL_HOST" --silent; do
+    #echo "Waiting for MariaDB to be ready..."
+    sleep 5
+done
+
+# Run SQL commands
+mysql -u root -p"$ROOT_PASS" <<EOSQL
+CREATE DATABASE ${WP_DB_NAME};
+CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
+GRANT ALL PRIVILEGES ON '${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOSQL
