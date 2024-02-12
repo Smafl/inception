370c845138c6791e8f77fedc89c7be8942241a1a
Elena Kulichkova <ekulichk@2-H-9.42heilbronn.de>
2024-02-12 19:24:18 +0100

    redo mariadb with debian, start nginx dockerfile

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-# Wait for MariaDB to be ready
-while ! mysqladmin ping -h"$MYSQL_HOST" --silent; do
-    #echo "Waiting for MariaDB to be ready..."
-    sleep 5
-done
+# Start MySQL service in the background
+service mariadb start
 
-# Run SQL commands
-mysql -u root -p"$ROOT_PASS" <<EOSQL
-CREATE DATABASE ${WP_DB_NAME};
-CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
-GRANT ALL PRIVILEGES ON '${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';
-FLUSH PRIVILEGES;
-EOSQL
+# Create MySQL user and grant privileges
+mysql -u root -p$ROOT_PASS -e "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME};"
+mysql -u root -p$ROOT_PASS -e "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';"
+mysql -u root -p$ROOT_PASS -e "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';"
+mysql -u root -p$ROOT_PASS -e "FLUSH PRIVILEGES;"
+
+# Stop MySQL service in the background
+mysqladmin -u root -p$ROOT_PASS shutdown
+
+# Run MySQL service in the foreground
+mysqld_safe
